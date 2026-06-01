e64317eb22b4ae2650a56c81d07c9c21fa82e184
Tilan <219650729+DilnukaTilan@users.noreply.github.com>
2026-06-01 14:33:36 +0530

    Add BookEvent component and booking UI

---
+++ components/BookEvent.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useState } from "react";
+
+const BookEvent = () => {
+  const [email, setEmail] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setSubmitted(true);
+    console.log(email);
+  };
+
+  return (
+    <div id="book-event">
+      {submitted ? (
+        <p className="text-sm">Thank you for signing up!</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="email">Enter your email to book your spot:</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email"
+            />
+            <button type="submit" className="button-submit">
+              Book Your Spot
+            </button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default BookEvent;
